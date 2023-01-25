9ff6affab5710a946335205021e2c1b4628ecbd2
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-01-25 22:49:01 +0530

    done 2

--- src/components/Scrollbar.js
+++ src/components/Scrollbar.js
@@ -20,6 +20,7 @@ export default function Scrollbar() {
                 spaceBetween={30}
                 loop={true}
                 centeredSlides={true}
+
                 autoplay={{
                     delay: 2500,
                     disableOnInteraction: false,
@@ -48,7 +49,7 @@ export default function Scrollbar() {
             >
                 <SwiperSlide>
                     <Card style={{ minHeight: "440px", width: '550px', backgroundColor: 'rgb(57 62 70)', color: "#eeeeee", borderRadius: "20px", border: '1.3mm ridge rgb(34 40 49)' }} >
-                        <Card.Img variant="top" src='https://i.postimg.cc/yYDmBc6J/Mr-Sahil-Jivan-Kothekar-3-png.webp' style={{ height: "300px", borderRadius: "20px 20px 0 0" }} />
+                        <Card.Img variant="top" src='https://i.postimg.cc/yYDmBc6J/Mr-Sahil-Jivan-Kothekar-3-png.webp' style={{ height: "360px", borderRadius: "20px 20px 0 0" }} />
                         <Card.Body style={{ height: "210px" }}>
                             <Card.Title style={{ fontSize: "18px" }}>Mr.Sahil Jivan Kothekar</Card.Title>
                             <Card.Text style={{ fontSize: "15px" }}>
@@ -60,7 +61,7 @@ export default function Scrollbar() {
                 </SwiperSlide>
                 <SwiperSlide>
                     <Card style={{ minHeight: "440px", width: '550px', backgroundColor: 'rgb(57 62 70)', color: "#eeeeee", borderRadius: "20px", border: '1.3mm ridge rgb(34 40 49)' }} >
-                        <Card.Img variant="top" src="https://i.postimg.cc/vBG7dKDM/Mr-Devashish-Sontakke-3-png.webp" style={{ height: "300px", borderRadius: "20px 20px 0 0" }} />
+                        <Card.Img variant="top" src="https://i.postimg.cc/vBG7dKDM/Mr-Devashish-Sontakke-3-png.webp" style={{ height: "360px", borderRadius: "20px 20px 0 0" }} />
                         <Card.Body style={{ height: "210px" }}>
                             <Card.Title style={{ fontSize: "18px" }}>Mr.Devashish Sontakke</Card.Title>
                             <Card.Text style={{ fontSize: "15px" }}>
@@ -71,7 +72,7 @@ export default function Scrollbar() {
                 </SwiperSlide>
                 <SwiperSlide>
                     <Card style={{ minHeight: "440px", width: '550px', backgroundColor: 'rgb(57 62 70)', color: "#eeeeee", borderRadius: "20px", border: '1.3mm ridge rgb(34 40 49)' }} >
-                        <Card.Img variant="top" src='https://i.postimg.cc/63HZx5DL/Mr-Swanand-Jugade-3-png.webp' style={{ height: "300px", borderRadius: "20px 20px 0 0" }} />
+                        <Card.Img variant="top" src='https://i.postimg.cc/63HZx5DL/Mr-Swanand-Jugade-3-png.webp' style={{ height: "360px", borderRadius: "20px 20px 0 0" }} />
                         <Card.Body style={{ height: "210px" }}>
                             <Card.Title style={{ fontSize: "18px" }}>Mr.Swanand Jugade</Card.Title>
                             <Card.Text style={{ fontSize: "15px" }}>
@@ -82,7 +83,7 @@ export default function Scrollbar() {
                 </SwiperSlide>
                 <SwiperSlide>
                     <Card style={{ minHeight: "440px", width: '550px', backgroundColor: 'rgb(57 62 70)', color: "#eeeeee", borderRadius: "20px", border: '1.3mm ridge rgb(34 40 49)' }} >
-                        <Card.Img variant="top" src="https://i.postimg.cc/2SB7dvPp/Dhiraj-Nichant-png.webp" style={{ height: "300px", borderRadius: "20px 20px 0 0" }} />
+                        <Card.Img variant="top" src="https://i.postimg.cc/2SB7dvPp/Dhiraj-Nichant-png.webp" style={{ height: "360px", borderRadius: "20px 20px 0 0" }} />
                         <Card.Body style={{ height: "210px" }}>
                             <Card.Title style={{ fontSize: "18px" }}>Mr.Dhiraj Nichant</Card.Title>
                             <Card.Text style={{ fontSize: "15px" }}>
@@ -92,7 +93,7 @@ export default function Scrollbar() {
                     </Card>
                 </SwiperSlide>
                 <SwiperSlide><Card style={{ minHeight: "440px", width: '550px', backgroundColor: 'rgb(57 62 70)', color: "#eeeeee", borderRadius: "20px", border: '1.3mm ridge rgb(34 40 49)' }} >
-                    <Card.Img variant="top" src='https://i.postimg.cc/s2YPqzjw/Mr-Aayush-Ulhas-Deshmukh-3-png.webp' style={{ height: "300px", borderRadius: "20px 20px 0 0" }} />
+                    <Card.Img variant="top" src='https://i.postimg.cc/s2YPqzjw/Mr-Aayush-Ulhas-Deshmukh-3-png.webp' style={{ height: "360px", borderRadius: "20px 20px 0 0" }} />
                     <Card.Body style={{ height: "210px" }}>
                         <Card.Title style={{ fontSize: "18px" }}>Mr.Ayush Ulhas Deshmukh</Card.Title>
                         <Card.Text style={{ fontSize: "15px" }}>
@@ -101,7 +102,7 @@ export default function Scrollbar() {
                     </Card.Body>
                 </Card></SwiperSlide>
                 <SwiperSlide><Card style={{ minHeight: "440px", width: '550px', backgroundColor: 'rgb(57 62 70)', color: "#eeeeee", borderRadius: "20px", border: '1.3mm ridge rgb(34 40 49)' }} >
-                    <Card.Img variant="top" src="https://i.postimg.cc/ZnCdbzmk/MRAnkit-Jamaiwar-png.webp" style={{ height: "300px", borderRadius: "20px 20px 0 0" }} />
+                    <Card.Img variant="top" src="https://i.postimg.cc/ZnCdbzmk/MRAnkit-Jamaiwar-png.webp" style={{ height: "360px", borderRadius: "20px 20px 0 0" }} />
                     <Card.Body style={{ height: "210px" }}>
                         <Card.Title style={{ fontSize: "18px" }}>Mr.Ankit Jamaiwar</Card.Title>
                         <Card.Text style={{ fontSize: "15px" }}>
@@ -110,7 +111,7 @@ export default function Scrollbar() {
                     </Card.Body>
                 </Card></SwiperSlide>
                 <SwiperSlide><Card style={{ minHeight: "440px", width: '550px', backgroundColor: 'rgb(57 62 70)', color: "#eeeeee", borderRadius: "20px", border: '1.3mm ridge rgb(34 40 49)' }} >
-                    <Card.Img variant="top" src='https://i.postimg.cc/Zn2y0sYW/Ms-Akshita-Sukesh-Baghel-3-png.webp' style={{ height: "300px", borderRadius: "20px 20px 0 0" }} />
+                    <Card.Img variant="top" src='https://i.postimg.cc/Zn2y0sYW/Ms-Akshita-Sukesh-Baghel-3-png.webp' style={{ height: "360px", borderRadius: "20px 20px 0 0" }} />
                     <Card.Body style={{ height: "210px" }}>
                         <Card.Title style={{ fontSize: "18px" }}>Ms Akshita Sukesh Baghel</Card.Title>
                         <Card.Text style={{ fontSize: "15px" }}>
@@ -119,7 +120,7 @@ export default function Scrollbar() {
                     </Card.Body>
                 </Card></SwiperSlide>
                 <SwiperSlide><Card style={{ minHeight: "440px", width: '550px', backgroundColor: 'rgb(57 62 70)', color: "#eeeeee", borderRadius: "20px", border: '1.3mm ridge rgb(34 40 49)' }} >
-                    <Card.Img variant="top" src='https://i.postimg.cc/K8XM2WsV/snheal-jpg.webp' style={{ height: "300px", borderRadius: "20px 20px 0 0" }} />
+                    <Card.Img variant="top" src='https://i.postimg.cc/K8XM2WsV/snheal-jpg.webp' style={{ height: "360px", borderRadius: "20px 20px 0 0" }} />
                     <Card.Body style={{ height: "210px" }}>
                         <Card.Title style={{ fontSize: "18px" }}>Ms Snheal joshi</Card.Title>
                         <Card.Text style={{ fontSize: "15px" }}>
@@ -132,7 +133,7 @@ export default function Scrollbar() {
                     </Card.Body>
                 </Card></SwiperSlide>
                 <SwiperSlide><Card style={{ minHeight: "440px", width: '550px', backgroundColor: 'rgb(57 62 70)', color: "#eeeeee", borderRadius: "20px", border: '1.3mm ridge rgb(34 40 49)' }} >
-                    <Card.Img variant="top" src="https://i.postimg.cc/m2CLrq87/hardik-bhaiya-3.jpg" style={{ height: "300px", borderRadius: "20px 20px 0 0" }} />
+                    <Card.Img variant="top" src="https://i.postimg.cc/m2CLrq87/hardik-bhaiya-3.jpg" style={{ height: "360px", borderRadius: "20px 20px 0 0" }} />
                     <Card.Body style={{ height: "210px" }}>
                         <Card.Title style={{ fontSize: "18px" }}>Mr.Ankit Jamaiwar</Card.Title>
                         <Card.Text style={{ fontSize: "15px" }}>
