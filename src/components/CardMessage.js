325d9beee7398cb462c925d2087aeeed1f9f1aaf
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-10-07 22:26:35 +0530

    first commit

--- src/components/CardMessage.js
+++ src/components/CardMessage.js
@@ -45,18 +45,18 @@ function CardMessage() {
 
 
             <Card className='card d-flex align-self-center ' style={{ borderRadius: "20px", backgroundColor: 'rgb(57 62 70)', color: "#eeeeee" }}>
-                <Card.Img className="bd-placeholder-img  w-50 h-100 " variant="top" src="https://i.postimg.cc/MTrnr3Q9/president.webp" style={{ borderRadius: "32px", border: '1.3mm ridge #eeeeee', marginTop: "20px" }} />
+                <Card.Img className="bd-placeholder-img  w-50 h-100 " variant="top" src="https://i.postimg.cc/mkrT6XJ8/President-mesa.png" style={{ borderRadius: "32px", border: '1.3mm ridge #eeeeee', marginTop: "20px" }} />
                 <Card.Body>
                     <Card.Title><h3>President's message</h3></Card.Title>
                     <Card.Text style={{ textAlign: "none" }}>
 
-                        Being the president of mesa i aim towards providing healthy environment in our department among all four year student  and also to provide technical knowledge that i have gained by exposing myself to various domains. We as mesa have actively proved to be the best branch in previous years and i wish to continue
-                        The legacy.{' '}
+                        Being the President of Mesa i aim that all the students of mechanical branch lead in every aspect of college whether it is Sports,Studies or extra circular activities.
+                        Whatever mistakes we made should not be repeated by them,and providing them with new opportunities so they can carve their personality and achive new milestones.{' '}
                     </Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flush" style={{ width: "100%", borderRadius: "0px 0 32px 32px", fontWeight: "600" }}>
-                    <ListGroup.Item > Email: ashutosh13052001@gmail.com</ListGroup.Item>
-                    <ListGroup.Item>Contact: +918669071583</ListGroup.Item>
+                    <ListGroup.Item > Email: khandaitmaurya@gmail.com</ListGroup.Item>
+                    <ListGroup.Item>Contact: +919960012577</ListGroup.Item>
 
                 </ListGroup>
             </Card>
